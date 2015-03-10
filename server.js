65fe994560bd878271f6b59ad8a5530819d5cf69
Conor Flannigan <cflanni1@jhu.edu>
2015-03-10 10:08:47 -0700

    Implement backend (incl. mongo and RESTful api)

---
+++ server.js
@@ -0,0 +1,29 @@
+var handler = require('./lib/request-handler');
+var cors = require('cors');
+var express = require('express');
+var app = express();
+app.use(cors());
+
+/* Our server is going to expose a RESTful API to allow for easy access of
+ user data sets.
+
+ Data sets are stored in GeoJSON format in our Mongo database. Each set is
+ represented by a 'feature collection.' Features are defined by users and
+ may contain arbitrary information relative to specific data points.
+ */
+
+// TODO: still need to handle user login, signup, auth
+//app.get('/', handler.renderIndex);
+
+app.get('/api/dataset', handler.getSets);
+app.get('/api/dataset/:id', handler.getSet);
+
+app.post('/api/dataset', handler.postSet);
+app.post('/api/point', handler.postPoint);
+
+// app.get('/*', function(req, res) {
+//   res.redirect('/');
+// });
+
+app.listen(3000);
+console.log('Server listening on port 3000');
